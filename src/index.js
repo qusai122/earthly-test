e47b4678577bbc5d96bafdab59309ae34613edc2
qusai122 <qusai.2010.123@hotmail.com>
2023-08-23 03:03:23 +0300

    Create the validation middleware

--- src/index.js
+++ src/index.js
@@ -12,16 +12,30 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.post("/reverse", (req, res) => {
-  if (!req.body.message) {
+function validateReversMessage(req, res, next) {
+  const message = req.body.message;
+
+  if (!message || typeof message !== "string") {
+    return res.status(400).json({ error: "Invalid message format" });
+  }
+  if (message.length < 2 || message.length > 100) {
     return res
       .status(400)
-      .json({ error: "Missing message field in the request body" });
+      .json({ error: "Message length should be between (2,100)" });
   }
 
+  next();
+}
+
+app.post("/reverse", validateReversMessage, (req, res) => {
   const reversedMessage = req.body.message.split("").reverse().join("");
 
   res.json({ reversedMessage });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Server Error" });
 });
 
 const PORT = 3000;
