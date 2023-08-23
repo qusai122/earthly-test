fd3a549daccc3f3591d3c063c8fa85b55c5e5055
qusai122 <qusai.2010.123@hotmail.com>
2023-08-23 19:28:54 +0300

    Refactore the errors handeler

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import express from "express";
 import { reverseRouters } from "./routers/index.js";
-import { loggingMiddleware } from "./middlewares/index.js";
+import {
+  loggingMiddleware,
+  errorHandler,
+  notFoundHandler,
+} from "./middlewares/index.js";
+
 const app = express();
+
 app.use(loggingMiddleware);
 app.use(express.json());
-
+app.use(errorHandler);
 app.use("/reverse", reverseRouters);
-
-app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-    return res.status(400).json({ error: "Invalid JSON" });
-  }
-  console.error(err.stack);
-  res.status(500).json({ error: "Server Error" });
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Endpoint not found" });
-});
+app.use(notFoundHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
