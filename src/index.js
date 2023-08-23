ae60b0f697767d3c8d3889e898825850115e8fd1
qusai122 <qusai.2010.123@hotmail.com>
2023-08-23 18:43:14 +0300

    Handle corrupted json

--- src/index.js
+++ src/index.js
@@ -47,6 +47,9 @@ app.post("/reverse", validateReversMessage, (req, res) => {
 });
 
 app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).json({ error: "Invalid JSON" });
+  }
   console.error(err.stack);
   res.status(500).json({ error: "Server Error" });
 });
