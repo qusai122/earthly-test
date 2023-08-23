911f8765a699d50911df8c2fdcd450552ece366b
qusai122 <qusai.2010.123@hotmail.com>
2023-08-23 03:04:47 +0300

    Handle non-existing endpoints

--- src/index.js
+++ src/index.js
@@ -38,6 +38,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Server Error" });
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Endpoint not found" });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
